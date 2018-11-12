6f9e2a4eec407229ed66aa06424363ad4ddae25f
Alex <alex@wendtedesigns.com>
2018-11-11 23:13:25 -0600

    version 0.4.1

--- client/src/header/__tests__/LinkGroup.tsx
+++ client/src/header/__tests__/LinkGroup.tsx
@@ -9,12 +9,48 @@ interface IManageBackgroundProps {
   setOpen: any
 }
 
-it('should render', () => {
+const setup = (propOverrides?: IProps) => {
+  const props = Object.assign({}, propOverrides)
+
   const manageBackground: IManageBackgroundProps = {
     liRef: {},
     navRef: {},
     setCoords: jest.fn(),
     setOpen: jest.fn(),
   }
-  render(<LinkGroup title="hi" manageBackground={manageBackground} />)
+
+  const utils = render(<LinkGroup title="test" manageBackground={manageBackground} />)
+  const liNode = utils.getByText('test')
+
+  return {
+    liNode,
+    manageBackground,
+    ...utils,
+  }
+}
+
+/* it('should show dropdown when LinkGroup on mouseEnter', () => {
+  const { manageBackground, liNode } = setup()
+  fireEvent.mouseEnter(liNode, { relatedTarget: liNode.firstChild })
+  expect(manageBackground.setOpen).toHaveBeenCalledWith(true)
+}) */
+it('should hide the dropdown on mouseLeave', () => {
+  const { manageBackground, liNode } = setup()
+  fireEvent.mouseLeave(liNode)
+  expect(manageBackground.setOpen).toHaveBeenCalledWith(false)
+})
+/* it('should show dropdown when LinkGroup on focus', () => {
+  const { manageBackground, liNode } = setup()
+  fireEvent.focus(liNode)
+  expect(manageBackground.setOpen).toHaveBeenCalledWith(true)
+}) */
+it('should hide the dropdown on blur', () => {
+  const { manageBackground, liNode } = setup()
+  fireEvent.blur(liNode)
+  expect(manageBackground.setOpen).toHaveBeenCalledWith(false)
+})
+it('should close dropdown when LinkGroup is clicked ', () => {
+  const { manageBackground, liNode } = setup()
+  fireEvent.click(liNode)
+  expect(manageBackground.setOpen).toHaveBeenCalledWith(false)
 })
